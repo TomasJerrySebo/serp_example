5d134cd69becab914959f2b54f85ba15acdb6467
unknown <tomas.sebo@NBSEBO.eset.corp>
2017-06-06 18:41:16 +0200

    Front-end build done

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,6 +6,7 @@
 
       	init: function () {
             this.loadTours();
+            this.appendEvents();
       	},
       	loadTours: function() {
       		request = $.ajax({
@@ -23,7 +24,36 @@
                     textStatus, errorThrown
                   );
             }); 
-      	}
+      	},
+        appendEvents: function() { 
+          // appending Sort by: to select
+          ($(window).width() < 992) ? $('select option').each(function(){ $(this).prepend('Sort by: '); }) : '';
+          $(window).resize(function(){ 
+            ($(window).width() < 992) ? $('select option').each(function(){ ($(this).text().indexOf('Sort by: ') < 0) ? $(this).prepend('Sort by: ') : ''; }) : $('select option').each(function(){ var text = $(this).text().replace('Sort by: ', ''); $(this).text(text); });
+          });
+
+          var minVal = 0;
+          var maxVal = 100;
+          $( function() {
+            $( "#slider-range" ).slider({
+              range: true,
+              min: minVal,
+              max: maxVal,
+              values: [ minVal, maxVal ],
+              slide: function( event, ui ) {
+                 $( "#amount-min" ).html(ui.values[ 0 ]+"days");
+                 $( "#amount-max" ).html(ui.values[ 1 ]+"days");
+              }
+            });
+            $( "#amount-min" ).html($( "#slider-range" ).slider( "values", 0 ) +"days");
+            $( "#amount-max" ).html($( "#slider-range" ).slider( "values", 1 ) +"days");
+          } );
+
+
+          $( function() {
+            $( "#datepicker" ).datepicker();
+          } );
+        }
 
   	} // end DiagramartProfile class
        
